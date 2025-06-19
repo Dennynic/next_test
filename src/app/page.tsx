84b49061a8e8b09cc560f7162887a28f782961df
Dmitriy Nikiforov <joragdetibil@gmail.com>
2025-06-19 23:14:43 +0500

    Add goods cards and quantity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReviewsSection from "@/components/Review/ReviewSection";
 import { Layout } from "@/components/Layout";
 import { Cart } from "@/components/Cart";
+import { GoodSection } from "@/components/GoodsSection";
 
 export default function Home() {
   return (
     <Layout>
       <ReviewsSection />
-      <Cart/>
+      <Cart />
+      <GoodSection />
     </Layout>
   );
 }
